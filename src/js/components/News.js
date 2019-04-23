868d0d70a51688ccc6b8f60bac1a7179af708e1f
Ramasee <Ramasee@yahoo.com>
2019-04-22 20:09:19 -0400

    Update News.js

--- src/js/components/News.js
+++ src/js/components/News.js
@@ -7,6 +7,11 @@ const News = () =>
     <div className={css.heading}>Latest updates</div>
       <div className={css.item}>
       <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
+      <div className={css.itemDate}>April 22, 2019</div>
+      <span className={css.itemText}>Updated to current status/proposals</span>
+    </div>
+      <div className={css.item}>
+      <span className={css.itemIcon}><IconChevronRight classes="icon--small" name="right chevron icon" nameSlug="right-chevron-icon" /></span>
       <div className={css.itemDate}>April 9, 2019</div>
       <span className={css.itemText}>Removed all the code name stuff.</span>
     </div>
