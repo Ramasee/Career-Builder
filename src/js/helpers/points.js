3dc2c6cf96841e250076b7098c47800b277602d7
Ramasee <Ramasee@yahoo.com>
2020-06-23 20:08:28 -0400

    Update points.js

--- src/js/helpers/points.js
+++ src/js/helpers/points.js
@@ -1,34 +1,49 @@
 // Calculate mastery points available based on char level and renown level
 export function calculateMasteryPoints(level, renown) {
   let points = 0;
-  if (level > 10) {
-    if (level > 20) {
-      points = level - 15;
-    } else {
-      if (level > 18) {
-        points = 5;
+      if (level > 21) {
+        points = 29;
+      } else if (level > 20) {
+        points = 26;
+      } else if (level > 19) {
+        points = 23;
+      } else if (level > 18) {
+        points = 20;
+      } else if (level > 17) {
+        points = 17;
       } else if (level > 16) {
-        points = 4;
+        points = 14;
+      } else if (level > 15) {
+        points = 11;
       } else if (level > 14) {
-        points = 3;
+        points = 8;
       } else if (level > 12) {
+        points = 7;
+      } else if (level > 10) {
+        points = 6;
+      } else if (level > 8) {
+        points = 5;
+      } else if (level > 6) {
+        points = 4;
+      } else if (level > 4) {
+        points = 3;
+      } else if (level > 2) {
         points = 2;
       } else {
         points = 1;
       }
-    }
     switch (renown) {
       case 40:
-        points = points + 1;
+        points = points + 0;
         break;
       case 50:
-        points = points + 2;
+        points = points + 0;
         break;
       case 60:
-        points = points + 3;
+        points = points + 0;
         break;
       case 70:
-        points = points + 4;
+        points = points + 0;
         break;
       default:
         break;
